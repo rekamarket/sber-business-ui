4e83c71d8ecaa4ee59ba51b9aeac0e16a41c2187
Poly Devil <polydevil@cocaine.ninja>
2022-05-31 10:19:54 +0300

    playroom: add font

--- .playroom/config.cjs
+++ .playroom/config.cjs
@@ -48,6 +48,18 @@ module.exports = {
           use: ["style-loader", "css-loader"],
         },
         {
+          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'fonts/'
+              }
+            }
+          ]
+        },
+        {
           test: /\.tsx?$/,
           exclude: /node_modules/,
           use: {
